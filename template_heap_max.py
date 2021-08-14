1d00666920fb7eb8a043ad47edd6ea8df63dde24
PacificDou <dsy.dlut@gmail.com>
2021-08-14 18:27:14 +0800

    support delete operation

--- template_heap_max.py
+++ template_heap_max.py
@@ -1,5 +1,6 @@
 
-def heapify(array, i, heapsize):
+# top down heapify
+def heapifyTopDownRecursion(array, i, heapsize):
     idxL = 2 * i + 1
     idxR = 2 * i + 2
     maxIdx = i
@@ -9,28 +10,57 @@ def heapify(array, i, heapsize):
         maxIdx = idxR
     if maxIdx != i:
         array[maxIdx], array[i] = array[i], array[maxIdx]
-        heapify(array, maxIdx, heapsize)
+        heapifyTopDown(array, maxIdx, heapsize)
+
+# top down heapify
+def heapifyTopDown(array, i, heapsize):
+    while True:
+        idxL = 2 * i + 1
+        idxR = 2 * i + 2
+        maxIdx = i
+        if idxL < heapsize and array[maxIdx] < array[idxL]:
+            maxIdx = idxL
+        if idxR < heapsize and array[maxIdx] < array[idxR]:
+            maxIdx = idxR
+        if maxIdx != i:
+            array[maxIdx], array[i] = array[i], array[maxIdx]
+            i = maxIdx
+        else:
+            break
+
+# bottom up heapify
+def heapifyBottomUp(array, i):
+    iP = (i - 1) // 2
+    while iP >= 0 and array[i] > array[iP]:
+        array[i], array[iP] = array[iP], array[i]
+        i = iP
+        iP = (i - 1) // 2
 
 def buildHeap(array):
     heapsize = len(array)
     for i in range(heapsize//2 - 1, -1, -1):
-        heapify(array, i, heapsize)
+        heapifyTopDown(array, i, heapsize)
 
 def push(array, item):
     array.append(item)
-    i = len(array) - 1
-    iP = (i - 1) // 2
-    while iP >= 0 and array[iP] < array[i]:
-        array[iP], array[i] = array[i], array[iP]
-        i = iP
-        iP = (i - 1) // 2
+    heapifyBottomUp(array, len(array) - 1)
 
 def pop(array):
     ret = array[0]
     array[0] = array[-1]
     del array[-1]
-    heapify(array, 0, len(array))
+    heapifyTopDown(array, 0, len(array))
     return ret
+
+def delete(array, i, heapsize):
+    array[i] = array[-1]
+    del array[-1]
+    heapsize -= 1
+    iP = (i - 1) // 2
+    if iP >= 0 and array[i] > array[iP]:
+        heapifyBottomUp(array, i)
+    else:
+        heapifyTopDown(array, i, heapsize)
 
 
 def heapSort(array):
@@ -38,6 +68,8 @@ def heapSort(array):
     n = len(array)
     ret = [pop(array) for i in range(n)]
     return ret
+
+
 
 
 
@@ -50,4 +82,14 @@ if __name__ == '__main__':
         push(heap, num)
     n = len(heap)
     print([pop(heap) for i in range(n)])
+
+    heap = []
+    for num in array:
+        push(heap, num)
+    print(heap)
+    delete(heap, 3, len(heap))
+    n = len(heap)
+    print([pop(heap) for i in range(n)])
+
+
 
--- template_heap_min.py
+++ template_heap_min.py
@@ -1,5 +1,6 @@
 
-def heapify(array, i, heapsize):
+# top down heapify
+def heapifyTopDownRecursion(array, i, heapsize):
     idxL = 2 * i + 1
     idxR = 2 * i + 2
     minIdx = i
@@ -11,27 +12,55 @@ def heapify(array, i, heapsize):
         array[minIdx], array[i] = array[i], array[minIdx]
         heapify(array, minIdx, heapsize)
 
+# top down heapify
+def heapifyTopDown(array, i, heapsize):
+    while True:
+        idxL = 2 * i + 1
+        idxR = 2 * i + 2
+        minIdx = i
+        if idxL < heapsize and array[minIdx] > array[idxL]:
+            minIdx = idxL
+        if idxR < heapsize and array[minIdx] > array[idxR]:
+            minIdx = idxR
+        if minIdx != i:
+            array[minIdx], array[i] = array[i], array[minIdx]
+            i = minIdx
+        else:
+            break
+
+# bottom up heapify
+def heapifyBottomUp(array, i):
+    iP = (i - 1) // 2
+    while iP >= 0 and array[i] < array[iP]:
+        array[i], array[iP] = array[iP], array[i]
+        i = iP
+        iP = (i - 1) // 2
+
 def buildHeap(array):
     heapsize = len(array)
     for i in range(heapsize//2 - 1, -1, -1):
-        heapify(array, i, heapsize)
+        heapifyTopDown(array, i, heapsize)
 
 def push(array, item):
     array.append(item)
-    i = len(array) - 1
-    idxParent = (i - 1) // 2
-    while idxParent >= 0 and array[idxParent] > array[i]:
-        array[idxParent], array[i] = array[i], array[idxParent]
-        i = idxParent
-        idxParent = (i - 1) // 2
+    heapifyBottomUp(array, len(array) - 1)
 
 def pop(array):
     ret = array[0]
     array[0] = array[-1]
     del array[-1]
-    heapify(array, 0, len(array))
+    heapifyTopDown(array, 0, len(array))
     return ret
 
+def delete(array, i, heapsize):
+    array[i] = array[-1]
+    del array[-1]
+    heapsize -= 1
+    iP = (i - 1) // 2
+    if iP >= 0 and array[i] < array[iP]:
+        heapifyBottomUp(array, i)
+    else:
+        heapifyTopDown(array, i, heapsize)
 
 def heapSort(array):
     buildHeap(array)
@@ -47,6 +76,14 @@ if __name__ == '__main__':
     heap = []
     for num in array:
         push(heap, num)
+    n = len(heap)
+    print([pop(heap) for i in range(n)])
+
+    heap = []
+    for num in array:
+        push(heap, num)
+    print(heap)
+    delete(heap, 3, len(heap))
     n = len(heap)
     print([pop(heap) for i in range(n)])
 
